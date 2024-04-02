d85bfdca31ff3510bdfe9d1c0b73189eb7c25dbc
Omolara5861 <72765403+Omolara5861@users.noreply.github.com>
2024-04-02 15:25:58 +0100

    Refactor js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,3 @@
-/*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
-  loop: true,
-  spaceBetween: 32,
-  grabCursor: true,
-
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  }
-});
-
-function selectOption(plan) {
-  const paymentPlans = document.querySelectorAll('.payment-plan');
-  const installmentBtn = document.getElementById('installmentPayment');
-  const oneTimeBtn = document.getElementById('oneTimePayment');
-    paymentPlans.forEach(plan => plan.classList.remove('active'));
-
-    if (plan === 'installment') {
-        document.getElementById('installment').classList.add('active');
-      installmentBtn.classList.add('active');
-      oneTimeBtn.classList.remove('active');
-    }
-    else {
-      document.getElementById('oneTime').classList.add('active');
-      oneTimeBtn.classList.add('active');
-      installmentBtn.classList.remove('active');
-    }
-  
-}
-
-
 document.addEventListener("DOMContentLoaded", function () {
   var scrollToTopBtn = document.getElementById("scrollToTop");
 
@@ -58,3 +20,44 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// Pricing
+function selectOption(plan) {
+  const paymentPlans = document.querySelectorAll('.payment-plan');
+  const installmentBtn = document.getElementById('installmentPayment');
+  const oneTimeBtn = document.getElementById('oneTimePayment');
+  paymentPlans.forEach(plan => plan.classList.remove('active'));
+
+  if (plan === 'installment') {
+    document.getElementById('installment').classList.add('active');
+    installmentBtn.classList.add('active');
+    oneTimeBtn.classList.remove('active');
+  }
+  else {
+    document.getElementById('oneTime').classList.add('active');
+    oneTimeBtn.classList.add('active');
+    installmentBtn.classList.remove('active');
+  }
+
+}
+
+
+/*=============== SWIPER JS ===============*/
+let swiperCards = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
+
+
